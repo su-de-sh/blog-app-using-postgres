0fc1db67c412ddae228940d1face4688b7ca0090
su-de-sh <sudesh7443@gmail.com>
2022-09-06 16:32:26 +0545

    fix: console.log made according to required

--- cli.js
+++ cli.js
@@ -16,7 +16,7 @@ const main = async ()=>{
 
   const blogs =  await sequelize.query("SELECT * FROM blogs", { type: QueryTypes.SELECT })
  
-  blogs.forEach(blog =>console.log(blog))
+  blogs.forEach(blog =>console.log(blog.author +":"+ "'"+blog.title +"'", blog.likes+"likes"))
 }
 
 
