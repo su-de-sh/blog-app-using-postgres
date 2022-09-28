3bb918b3991de1f03634e1d354293a90130d081f
su-de-sh <sudesh7443@gmail.com>
2022-09-28 16:15:19 +0545

    feat: add migrations to initialize blog and user

---
+++ migrations/20220923_00_jnitialize_user_and_blog_table.js
@@ -0,0 +1,68 @@
+const { DataTypes } = require("sequelize");
+module.exports = {
+  up: async ({ context: queryInterface }) => {
+    await queryInterface.createTable("blogs", {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      author: {
+        type: DataTypes.TEXT,
+      },
+      url: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      title: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      likes: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0,
+      },
+      updated_at: { type: DataTypes.DATE },
+      created_at: { type: DataTypes.DATE },
+      //   user_id: {
+      //     type: DataTypes.INTEGER,
+      //     allowNull: false,
+      //     references: { model: "users", key: "id" },
+      //   },
+    });
+    await queryInterface.createTable("users", {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      username: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        validate: {
+          isEmail: true,
+        },
+        error: ["Validation isEmail on username failed"],
+      },
+      name: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      password_hash: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      updated_at: { type: DataTypes.DATE },
+      created_at: { type: DataTypes.DATE },
+    });
+    await queryInterface.addColumn("blogs", "user_id", {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: { model: "users", key: "id" },
+    });
+  },
+  down: async ({ context: queryInterface }) => {
+    await queryInterface.dropTable("blogs");
+    await queryInterface.dropTable("users");
+  },
+};
--- utils/db.js
+++ utils/db.js
@@ -1,25 +1,42 @@
-const { Sequelize, Model, DataTypes,QueryTypes, BelongsTo } = require('sequelize');
-const { DATABASE_URL } = require('./config');
+const { Sequelize } = require("sequelize");
+const { Umzug, SequelizeStorage } = require("umzug");
+const { DATABASE_URL } = require("./config");
 
 const sequelize = new Sequelize(DATABASE_URL, {
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
     },
+  },
+});
+const runMigrations = async () => {
+  const migrator = new Umzug({
+    migrations: {
+      glob: "migrations/*.js",
+    },
+    storage: new SequelizeStorage({ sequelize, tableName: "migrations" }),
+    context: sequelize.getQueryInterface(),
+    logger: console,
   });
 
-  const connectToDatabase = async () => {
-    try {
-      await sequelize.authenticate()
-      console.log('connected to the database')
-    } catch (err) {
-      console.log('failed to connect to the database')
-      return process.exit(1)
-    }
-  
-    return null
+  const migrations = await migrator.up();
+  console.log("Migrations up to date", {
+    files: migrations.map((mig) => mig.name),
+  });
+};
+
+const connectToDatabase = async () => {
+  try {
+    await sequelize.authenticate();
+    await runMigrations();
+    console.log("connected to the database");
+  } catch (err) {
+    console.log("failed to connect to the database");
+    return process.exit(1);
   }
 
-  module.exports = {sequelize,connectToDatabase}
+  return null;
+};
+
+module.exports = { sequelize, connectToDatabase };
